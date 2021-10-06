ae0c481d82618511d46a57f3b49bab493740f0bd
Zhan Lun Ng <zlun0817@gmail.com>
2021-10-06 16:27:47 +0800

    Add signup route/controller

--- auth/authController.js
+++ auth/authController.js
@@ -1,6 +1,6 @@
 import db from '../db/index.js'
 import bcrypt from 'bcrypt'
-import { generateAccessToken } from './util.js'
+import { generateAccessToken, getHashedPassword } from './util.js'
 
 export const loginUser = async (req, res) => {
   // Authenticate User
@@ -9,13 +9,49 @@ export const loginUser = async (req, res) => {
   const { rows } = await db.query('SELECT * FROM app_user WHERE username = $1', [username])
 
   if (rows.length === 0) {
-    return res.status(401).send('Email or password is incorrect')
+    return res.status(401).send('Username or password is incorrect')
   }
   const validPassword = await bcrypt.compare(password, rows[0].password)
   if (!validPassword) {
-    return res.status(401).send('Email or password is incorrect')
+    return res.status(401).send('Username or password is incorrect')
   }
 
+  const user = {
+    username
+  }
+  const accessToken = generateAccessToken(user)
+  return res.json({ accessToken: accessToken })
+}
+
+export const signUpUser = async (req, res) => {
+  // Authenticate User
+  const { username, password } = req.body
+
+  const { rows } = await db.query('SELECT * FROM app_user WHERE username = $1', [username])
+
+  if (rows.length > 0) {
+    return res.status(409).send('Username already exists')
+  }
+
+  if (username.length < 4 || password.length < 4) {
+    return res.status(400).send('Invalid input')
+  }
+
+  const hashedPassword = await getHashedPassword(password)
+
+  try {
+    await db.query(
+      `
+        INSERT INTO app_user (username, password) VALUES ($1, $2)
+      `,
+      [
+        username,
+        hashedPassword,
+      ]
+    )
+  } catch (error) {
+    return res.status(500).json(error.message)
+  }
   const user = {
     username
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { loginUser } from '../auth/authController.js'
+import { loginUser, signUpUser } from '../auth/authController.js'
 
 const router = express.Router({ mergeParams: true })
 
 router.post('/login', loginUser)
+router.post('/signup', signUpUser)
 
 export default router
