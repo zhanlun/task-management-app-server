5304056329c0f48f42b9852a5e76f1c4dc15dc18
Zhan Lun Ng <zlun0817@gmail.com>
2021-10-10 11:12:03 +0800

    Add conditional config for dotenv

--- auth/util.js
+++ auth/util.js
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
-import dotenv from 'dotenv'
-dotenv.config()
+
+if (process.env.NODE_ENV !== 'production') {
+  import dotenv from 'dotenv'
+  dotenv.config()
+}
 
 const saltRound = 10
 
